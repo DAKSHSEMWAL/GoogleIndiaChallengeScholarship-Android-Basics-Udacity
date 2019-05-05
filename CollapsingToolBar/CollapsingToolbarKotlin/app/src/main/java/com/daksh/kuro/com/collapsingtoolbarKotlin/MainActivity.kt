a6e94a72a3ffec9210fb65af0fb0aa2a5a8eb83a
kuro.aquasoft <kuro.aquasoft@gmail.com>
2019-05-05 15:07:27 +0530

    05-05-2019

---
+++ CollapsingToolBar/CollapsingToolbarKotlin/app/src/main/java/com/daksh/kuro/com/collapsingtoolbarKotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.daksh.kuro.com.collapsingtoolbarKotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.daksh.kuro.com.collapsingtoolbarKotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
